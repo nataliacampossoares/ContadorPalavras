5cd43a41688f9fb727e68d97babbe54ae2fb3c0e
Natália Campos Soares <130059334+nataliacampossoares@users.noreply.github.com>
2024-04-24 11:55:04 -0300

    .

--- script.js
+++ script.js
@@ -5,25 +5,36 @@ const divContador = document.querySelector("[txt-contador]")
 let isContagemCaracteres = true
 
 function handleInputTexto(evento){
-    //let contadorString = evento.target.value.length;
-    //divContador.innerText = contadorString + " caracteres";
-    let contadorPalavras = evento.target.value.split(" ")
-    let contar = contadorPalavras.lenght;
-    divContador.innerText = contar + " caracteres";
+    console.log("entrou")
+    if (isContagemCaracteres){
+        let contadorString = evento.target.value.length;
+        divContador.innerText = contadorString + " caracteres";}
+    else {
+        let contadorPalavra = evento.target.value.trim().split(" ").length
+        divContador.innerText = contadorPalavra + " palavras"
+    }
+    contarAtualizar()
 }
 
 function handleBtnContar(evento){
     if (isContagemCaracteres){
         evento.target.innerText = "Contar Palavras"
-        // isContagemCaracteres = false
     }
     else{
         evento.target.innerText = "Contar Caracteres"
-        // isContagemCaracteres = true
     }
-
+    
     isContagemCaracteres = !isContagemCaracteres
+    
+    inputTexto.dispatchEvent(new Event("input")) //ele "entra" na funcao handleInputTexto
+    contarAtualizar()
+
 }
+
+function contarAtualizar() {
+
+}
+
 
 btnContar.addEventListener("click", handleBtnContar)
 inputTexto.addEventListener("input", handleInputTexto)
